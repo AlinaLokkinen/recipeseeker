4a552bbba08cde41faac4d04a58a344c493da4a7
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2025-09-05 10:44:08 +0300

    Home background

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+
+
+    return (
+        <div className="bg-black">
+            <div className="bg-[url('../public/images/bg.jpg')] bg-cover h-screen opacity-50">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Home;
