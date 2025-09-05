5051015dd5ac8369b8ff66bd148a4bd94984c73d
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2025-09-05 11:01:40 +0300

    Fonts added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,15 @@ const Home = () => {
 
 
     return (
-        <div className="bg-black">
-            <div className="bg-[url('../public/images/bg.jpg')] bg-cover h-screen opacity-50">
-            
+        <div>
+            <div className="bg-black relative h-screen">
+            <div className="bg-[url('../public/images/bg.jpg')] bg-cover h-screen opacity-50 absolute inset-0 ">
+                
             </div>
+            <div className="relative z-20 text-white font-playfair">
+                <h1 >Recipe Seeker</h1>
+            </div>
+        </div>
         </div>
     )
 }
